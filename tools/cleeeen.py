a66d15d0e3d45d5474cf328f7f97768847f058b0
sorlovski <sorlovski@atlassian.com>
2020-10-26 18:15:27 -0300

    add cleening script

---
+++ tools/cleeeen.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os
+import requests
+import json
+import send2trash
+
+path = "/Users/svetlozar.orlovski/Tickets"
+listdir = os.listdir(path)
+
+tickets = ""
+url = "https://ps-ticketstatus.prod.atl-paas.net/tickets"
+
+for folder in listdir:
+    if (folder.startswith("PS" or "BSP")):
+        tickets += folder + ","
+
+# Remove the last comma
+tickets = tickets[:-1]
+
+print(tickets)
+querystring = {"keys":tickets}
+
+response = requests.request("GET", url, params=querystring)
+
+print(response.text)
+print("-------------------------------")
+print(response.status_code)
+
+
+if response.status_code == 200:
+    print(response.text)
+    issues = json.loads(response.text)
+   #nremoved = 0
+
+    for issue in issues:
+        print(issue[u'key'] + " " + issue[u'status'])
+        if(issue[u'status'] == "Closed"):
+            send2trash.send2trash(path + "/" + issue[u'key'])
+            print("removed " + path + "/" + issue[u'key'] )
+            #nremoved = nremoved + 1
+    print("")
+    #print("removed " + nremoved)
+else:
+    print("Rest API failed with: " + response.status_code)
